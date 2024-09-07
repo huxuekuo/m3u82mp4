f74146d833433ca7a1deea00637474fd92808f5b
hu <hu@hudeMacBook-Pro.local>
2024-09-07 18:42:37 +0800

    init

--- api/main.go
+++ api/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
+
+	"github.com/chromedp/cdproto/cdp"
+	"github.com/chromedp/chromedp"
 )
 
 // 119.29.226.140:13457 172.247.47.125
 func main() {
-
 	http.HandleFunc("/query", func(w http.ResponseWriter, r *http.Request) {
 		key := r.FormValue("key")
 		other_kkk217 := "http://xdm530.com"
@@ -32,12 +37,63 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		// var datamap map[string]any
-		// err = json.Unmarshal(bytedata, &datamap)
-		// if err != nil {
-		// 	panic(err)
-		// }
 		w.Write(bytedata)
+
+	})
+	http.HandleFunc("/getInfo", func(w http.ResponseWriter, r *http.Request) {
+		urls := r.FormValue("url")
+		host := "http://xdm530.com"
+		newHostUrl := host + urls
+		// 禁用chrome headless
+		opts := append(chromedp.DefaultExecAllocatorOptions[:],
+			chromedp.Flag("headless", true),
+		)
+		allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+		defer cancel()
+		ctx, cancel := chromedp.NewContext(
+			allocCtx,
+		)
+		defer cancel()
+		ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
+		defer cancel()
+		var onClick []*cdp.Node
+		var aNodes []*cdp.Node
+		err := chromedp.Run(ctx, chromedp.Navigate(newHostUrl),
+			chromedp.WaitVisible("#tab12"),
+			chromedp.Nodes("body > div.wrap > div.taba-down.mb.clearfix > div.pfrom.tab1.clearfix > #play_list_34", &onClick),
+			chromedp.Nodes(":is(a)", &aNodes, chromedp.ByQueryAll))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		line := make(map[string]string, len(onClick[0].Children))
+		for _, v := range onClick[0].Children {
+			onClick, exits := v.Attribute("onclick")
+			if exits {
+				line[v.Children[0].NodeValue] = strings.Split(onClick, ",")[2]
+			}
+		}
+		fmt.Printf("%+v\n", line)
+		episodes := make(map[string]string, 0)
+		for _, node := range aNodes {
+			href, b := node.Attribute("href")
+			if b && strings.Contains(href, "html") {
+				if strings.Contains(href, "kb") {
+					fmt.Println("debug")
+				}
+				queryParse, err := url.Parse(href)
+				if err != nil {
+					continue
+				}
+				qp := queryParse.Query().Get("qp")
+				if qp == "" {
+					episodes["default"] = href
+				} else {
+					episodes[qp] = href
+				}
+			}
+		}
+
 	})
 	http.ListenAndServe(":8080", nil)
 }
